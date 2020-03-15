0a32ddf168d5e9299a303c462c11aba06c946f65
Vidyap23 <61863746+Vidyap23@users.noreply.github.com>
2020-03-15 18:15:56 +0530

    Update functions-and-arrays.js

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,4 +1,3 @@
-// Progression #1: Greatest of the two numbers
 function greatestOfTwoNumbers(num1, num2) {
   if (num1 > num2) return num1;
   else return num2;
@@ -8,6 +7,7 @@ function greaterOfTwoNumbers(num1, num2) {
   if (num1 > num2) return num1;
   else return num2;
 }
+
 
 
 // Progression #2: The lengthy word
@@ -24,7 +24,7 @@ const words = [
 function findScaryWord(words) {
   var lengthOfArray = words.length;
   var max_length = 0;
-  if (lenOfArray == 0) {
+  if (lengthOfArray == 0) {
     return null;
   }
   if (lengthOfArray == 1) {
@@ -85,133 +85,168 @@ function add(arr) {
     }
   }
   return sum;
+}
 
 
-  // Progression #4: Calculate the average
-  /*Progression 4.1: Array of numbers*/
 
-  const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
+// Progression #4: Calculate the average
+const arr = [2, 6, 9, 10, 7, 4, 1, 9];
 
-  function midPointOfLevels(numbersAvg) {
-    if (numbersAvg.length == 0) {
-      return null;
-    } else {
-      var average = netPrice(numbersAvg);
-      return average / arr.length;
-    }
+function midPointOfLevels(arr) {
+  if (arr.length == 0) {
+    return null;
   }
 
-
-  //Progression 2 array of strings
-
-  const wordsArr = [
-    "seat",
-    "correspond",
-    "linen",
-    "motif",
-    "hole",
-    "smell",
-    "smart",
-    "chaos",
-    "fuel",
-    "palace"
-  ]
-
-  function averageWordLength(wordsArr) {
-    var sum = 0;
-    if (wordsArr.length == 0) return null;
-    else {
-      for (var i = 0; i < wordsArr.length; i++) {
-        sum += wordsArr[i].length;
-      }
-      var x = sum / wordsArr.length;
-      return x;
-    }
+  else {
+    var sum = netPrice(arr);
   }
-
-  function avg(arr) {
-    var sum = 0;
-    if (arr.length == 0) {
-      return null;
-    } else {
-      for (let i = 0; i < arr.length; i++) {
-        if (typeof arr[i] == "string") {
-          let len = arr[i].length;
-          sum = sum + len;
-        } else if (typeof arr[i] == "boolean") {
-          var l = arr[i] / 1;
-          sum = sum + l;
-        } else if (typeof arr[i] == "number") {
-          sum = sum + arr[i];
-        } else {
-          throw exception(
-            "Unsupported data type (object or array) present in the array"
-          );
-        }
-      }
-    }
-    let avWrL = sum / arr.length;
-    return Number(avWrL.toFixed(2));
-  }
-
-  // Progression #5: Unique arrays
-  const wordsUnique = [
-    "bread",
-    "jam",
-    "milk",
-    "egg",
-    "flour",
-    "oil",
-    "rice",
-    "coffee powder",
-    "sugar",
-    "salt",
-    "egg",
-    "flour"
-  ];
-
-  function uniqueArray(array) {
-    // var unique = [];
-    if (array.length == 0) {
-      return null;
-    } else {
-      return array.filter((a, b) => array.indexOf(a) === b);
-    }
-
-
-    // Progression #6: Find elements
-    const wordsFind = ['machine', 'subset', 'trouble', 'starting', 'matter', 'eating', 'truth', 'disobedience'];
+  var average = sum / arr.length;
+  return average;
+}
 
 
 
-    // Progression #7: Count repetition
-    const wordsCount = [
-      'machine',
-      'matter',
-      'subset',
-      'trouble',
-      'starting',
-      'matter',
-      'eating',
-      'matter',
-      'truth',
-      'disobedience',
-      'matter'
-    ];
 
-    // Progression #8: Bonus
+//Progression 2 array of strings
 
-    const matrix = [
-      [08, 02, 22, 97, 38, 15, 00, 40, 00, 75],
-      [49, 49, 99, 40, 17, 81, 18, 57, 60, 87],
-      [81, 49, 31, 73, 55, 79, 14, 29, 93, 71],
-      [52, 70, 95, 23, 04, 60, 11, 42, 69, 24],
-      [22, 31, 16, 71, 51, 67, 63, 89, 41, 92],
-      [24, 47, 32, 60, 99, 03, 45, 02, 44, 75],
-      [32, 98, 81, 28, 64, 23, 67, 10, 26, 38],
-      [67, 26, 20, 68, 02, 62, 12, 20, 95, 63],
-      [24, 55, 58, 05, 66, 73, 99, 26, 97, 17],
-      [21, 36, 23, 09, 75, 00, 76, 44, 20, 45],
-    ]
+const wordsArr = [
+  "seat",
+  "correspond",
+  "linen",
+  "motif",
+  "hole",
+  "smell",
+  "smart",
+  "chaos",
+  "fuel",
+  "palace"
+]
+
+function averageWordLength(wordsArr) {
+  var sum = 0;
+  if (wordsArr.length == 0) return null;
+  else {
+
+    var avg = sum / wordsArr.length;
+    return avg;
   }
 }
+
+function avg(arr) {
+  var sum = 0;
+  if (arr.length == 0) {
+    return null;
+  } else {
+    for (let i = 0; i < arr.length; i++) {
+      if (typeof arr[i] == "string") {
+        let len = arr[i].length;
+        sum = sum + len;
+      } else if (typeof arr[i] == "boolean") {
+        var l = arr[i] / 1;
+        sum = sum + l;
+      } else if (typeof arr[i] == "number") {
+        sum = sum + arr[i];
+      } else {
+        throw exception(
+          "Unsupported data type (object or array) present in the array"
+        );
+      }
+    }
+  }
+  let avWrL = sum / arr.length;
+  return Number(avWrL.toFixed(2));
+}
+
+// Progression #5: Unique arrays
+const wordsUnique = [
+  "bread",
+  "jam",
+  "milk",
+  "egg",
+  "flour",
+  "oil",
+  "rice",
+  "coffee powder",
+  "sugar",
+  "salt",
+  "egg",
+  "flour"
+];
+
+function uniqueArray(array) {
+  // var unique = [];
+  if (array.length == 0) {
+    return null;
+  } else {
+    return array.filter((a, b) => array.indexOf(a) === b);
+  }
+}
+
+
+// Progression #6: Find elements
+const wordsFind = ['machine', 'subset', 'trouble', 'starting', 'matter', 'eating', 'truth', 'disobedience'];
+function searchElement(wordsFind, key) {
+  if (wordsFind = [])
+    return null;
+  else {
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] == key) {
+        return true;
+
+      }
+      else return false;
+    }
+
+
+
+  }
+}
+
+
+
+
+
+// Progression #7: Count repetition
+const wordsCount = [
+  'machine',
+  'matter',
+  'subset',
+  'trouble',
+  'starting',
+  'matter',
+  'eating',
+  'matter',
+  'truth',
+  'disobedience',
+  'matter'
+];
+function howManyTimesElementRepeated(array, word) {
+  var count = 0;
+  if (array.length == 0) {
+    return 0;
+  } else {
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] == word) {
+        count = count + 1;
+      }
+    }
+    return count;
+  }
+}
+
+// Progression #8: Bonus
+
+const matrix = [
+  [08, 02, 22, 97, 38, 15, 00, 40, 00, 75],
+  [49, 49, 99, 40, 17, 81, 18, 57, 60, 87],
+  [81, 49, 31, 73, 55, 79, 14, 29, 93, 71],
+  [52, 70, 95, 23, 04, 60, 11, 42, 69, 24],
+  [22, 31, 16, 71, 51, 67, 63, 89, 41, 92],
+  [24, 47, 32, 60, 99, 03, 45, 02, 44, 75],
+  [32, 98, 81, 28, 64, 23, 67, 10, 26, 38],
+  [67, 26, 20, 68, 02, 62, 12, 20, 95, 63],
+  [24, 55, 58, 05, 66, 73, 99, 26, 97, 17],
+  [21, 36, 23, 09, 75, 00, 76, 44, 20, 45],
+]
+
+
