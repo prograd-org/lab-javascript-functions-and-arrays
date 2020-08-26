005534132e54255357628dbc79312e1c53215fb1
surajdesai1297@gmail.com <suraj@LAPTOP-C83DOH9J.localdomain>
2020-08-26 12:22:39 +0530

    FSJ0820B107

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,64 +1,66 @@
 // Progression #1: Greatest of the two numbers
 function greatestOfTwoNumbers(number1, number2) {
   if (number1 > number2) {
-    return number1
+    return number1;
   } else if (number2 > number1) {
-    return number2
+    return number2;
   }
-  return number1
-
-
+  return number1;
 }
 // Progression #2: The lengthy word
-const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
+const words = [
+  "mystery",
+  "brother",
+  "aviator",
+  "crocodile",
+  "pearl",
+  "orchard",
+  "crackpot",
+];
 
 function findScaryWord(words) {
   if (!words.length) {
-    return null
+    return null;
   }
 
-  let longestWord = ""
-  let numberOfCharacters = 0
+  let longestWord = "";
+  let numberOfCharacters = 0;
 
   for (let word of words) {
-
     if (word.length > numberOfCharacters) {
-      longestWord = word
-      numberOfCharacters = longestWord.length
+      longestWord = word;
+      numberOfCharacters = longestWord.length;
     }
-
   }
-  return longestWord
+  return longestWord;
 }
 
 // Progression #3: Net Price
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
 function netPrice(prices) {
-
-  let sum = 0
+  let sum = 0;
   for (let price of prices) {
-    sum += price
+    sum += price;
   }
-  return sum
+  return sum;
 }
 
 // Progression #3.1 (Bonus): A generic sum() function
 function add(list) {
-
-  let sum = 0
+  let sum = 0;
 
   for (let item of list) {
     if (typeof item == "string") {
-      sum += item.length
+      sum += item.length;
     } else if (typeof item == "number" || typeof item == "boolean") {
-      sum += item
+      sum += item;
     } else {
       throw new Error("Unsupported data type sir or ma'am");
     }
   }
 
-  return sum
+  return sum;
 }
 
 // Progression #4: Calculate the average
@@ -66,108 +68,124 @@ function add(list) {
 const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
 
 function midPointOfLevels(list) {
-  const avg = add(list) / list.length
+  const avg = add(list) / list.length;
   if (avg) {
-    return avg
+    return avg;
   }
-  return null
+  return null;
 }
 
-
-
 // Progression 4.2: Array of strings
-const wordsArr = ['seat', 'correspond', 'linen', 'motif', 'hole', 'smell', 'smart', 'chaos', 'fuel', 'palace'];
+const wordsArr = [
+  "seat",
+  "correspond",
+  "linen",
+  "motif",
+  "hole",
+  "smell",
+  "smart",
+  "chaos",
+  "fuel",
+  "palace",
+];
 
 function averageWordLength(list) {
-  return midPointOfLevels(list)
+  return midPointOfLevels(list);
 }
 
 // Progression #4.3 (Bonus): A generic avg() function
 function avg(list) {
-  let avg = midPointOfLevels(list)
+  let avg = midPointOfLevels(list);
   if (avg) {
-    return parseFloat(avg.toFixed(2))
+    return parseFloat(avg.toFixed(2));
   }
-  return avg
+  return avg;
 }
 
 // Progression #5: Unique arrays
 const wordsUnique = [
-  'bread',
-  'jam',
-  'milk',
-  'egg',
-  'flour',
-  'oil',
-  'rice',
-  'coffee powder',
-  'sugar',
-  'salt',
-  'egg',
-  'flour'
+  "bread",
+  "jam",
+  "milk",
+  "egg",
+  "flour",
+  "oil",
+  "rice",
+  "coffee powder",
+  "sugar",
+  "salt",
+  "egg",
+  "flour",
 ];
 
 function uniqueArray(list) {
-  let uniqueIndex = []
+  let uniqueIndex = [];
   for (let item of list) {
-    let index = list.indexOf(item)
+    let index = list.indexOf(item);
     if (uniqueIndex.indexOf(index) == -1) {
-      uniqueIndex.push(index)
+      uniqueIndex.push(index);
     }
   }
 
   if (!uniqueIndex.length) {
-    return null
+    return null;
   }
-  let uniqueList = []
+  let uniqueList = [];
 
   for (let index of uniqueIndex) {
-    uniqueList.push(list[index])
+    uniqueList.push(list[index]);
   }
-  return uniqueList
+  return uniqueList;
 }
 
 // Progression #6: Find elements
-const wordsFind = ['machine', 'subset', 'trouble', 'starting', 'matter', 'eating', 'truth', 'disobedience'];
+const wordsFind = [
+  "machine",
+  "subset",
+  "trouble",
+  "starting",
+  "matter",
+  "eating",
+  "truth",
+  "disobedience",
+];
 
 function searchElement(list, search) {
   if (!list.length) {
-    return null
+    return null;
   }
   for (let word of list) {
     if (word == search) {
-      return true
+      return true;
     }
   }
-  return false
+  return false;
 }
-
 
 // Progression #7: Count repetition
 const wordsCount = [
-  'machine',
-  'matter',
-  'subset',
-  'trouble',
-  'starting',
-  'matter',
-  'eating',
-  'matter',
-  'truth',
-  'disobedience',
-  'matter'
+  "machine",
+  "matter",
+  "subset",
+  "trouble",
+  "starting",
+  "matter",
+  "eating",
+  "matter",
+  "truth",
+  "disobedience",
+  "matter",
 ];
 
 function howManyTimesElementRepeated(list, search) {
-  let count = 0
+  let count = 0;
   for (let item of list) {
     if (item == search) {
-      count++
+      count++;
     }
   }
-  return count
+  return count;
 }
-
 
 // Progression #8: Bonus
 
@@ -181,55 +199,66 @@ const matrix = [
   [32, 98, 81, 28, 64, 23, 67, 10, 26, 38],
   [67, 26, 20, 68, 02, 62, 12, 20, 95, 63],
   [24, 55, 58, 05, 66, 73, 99, 26, 97, 17],
-  [21, 36, 23, 09, 75, 00, 76, 44, 20, 45]
+  [21, 36, 23, 09, 75, 00, 76, 44, 20, 45],
 ];
 
 function maximumProduct(matrix) {
-  let result = 0
+  let result = 0;
 
-  for (let i = 0; i <= matrix[0].length - 4; i++) {
+  for (let i = 0; i <= matrix.length - 4; i++) {
     let product1 = 1;
     let product2 = 1;
+    for (let k = 0; k < matrix[0].length; k++) {
+      // horizontaly
+      for (let j = k; j <= k + 3; j++) {
+        if (matrix[i][j]) {
+          product1 *= matrix[i][j];
+        }
+      }
 
-    // diagonally
-    for (let j = i; j <= i + 3; j++) {
-      product1 *= matrix[i][j]
-    }
+      // vertically
+      for (let j = 0; j <= 3; j++) {
+        product2 *= matrix[i + j][k];
+        console.log(matrix[i + j][k]);
+      }
 
-    // vertically
-    for (let j = i; j <= i + 3; j++) {
-      product2 *= matrix[j][i]
-    }
-
-    if (product1 > product2) {
-      result = product1
-    } else {
-      result = product2
+      if (product1 > product2) {
+        result = product1;
+      } else {
+        result = product2;
+      }
     }
   }
-
-  return result
+  return result;
 }
-
 
 // Progression #8.1 (Bonus): Product of diagonals
 function maximumProductOfDiagonals(matrix) {
-  let result = 0
+  let result = 0;
 
   for (let i = 0; i <= matrix.length - 4; i++) {
-    let product1 = matrix[i][i] * matrix[i + 1][i + 1] * matrix[i + 2][i + 2] * matrix[i + 3][i + 3]
+    let product =
+      matrix[i][i] *
+      matrix[i + 1][i + 1] *
+      matrix[i + 2][i + 2] *
+      matrix[i + 3][i + 3];
 
-    let product2 = 1
-    for (let j = 1; j <= 4; j++) {
-      product2 *= matrix[i][matrix.length - j - i]
-    }
-
-    if (product1 > product2) {
-      result = product1
-    } else {
-      result = product2
+    if (product > result) {
+      result = product;
     }
   }
 
-  return product
+  for (let i = 0; i <= matrix.length - 4; i++) {
+    let product = 1;
+    count = 0;
+    while (count < 4) {
+      product *= matrix[i + count][[matrix[0].length - 1] - (i + count)];
+      count++;
+    }
+    if (product > result) {
+      result = product;
+    }
+  }
+
+  return result;
 }
