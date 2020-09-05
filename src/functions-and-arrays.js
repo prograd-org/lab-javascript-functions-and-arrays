6d2416f33923b859680e7b82daed861dbc68de7c
digbijay1 <digbijay.das@suiit.ac.in>
2020-09-05 09:28:34 -0700

    2 nd commit

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -255,3 +255,48 @@ const matrix = [
   [24, 55, 58, 05, 66, 73, 99, 26, 97, 17],
   [21, 36, 23, 09, 75, 00, 76, 44, 20, 45]
 ];
+function maximumProduct(matrix){
+  var max = 0, result; 
+  
+  // iterate the rows. 
+  for (var i = 0; i < n; i++)  
+  { 
+  // iterate the columns. 
+  for (var j = 0; j < n; j++)  
+  { 
+      // check the maximum product 
+      // in horizontal row. 
+      if ((j - 3) >= 0)  
+      { 
+      result = arr[i][j] * arr[i][j - 1] *  
+              arr[i][j - 2] * arr[i][j - 3]; 
+      if (max < result) 
+          max = result; 
+      } 
+
+      // check the maximum product 
+      // in vertical row. 
+      if ((i - 3) >= 0)  
+      { 
+      result = arr[i][j] * arr[i - 1][j] *  
+              arr[i - 2][j] * arr[i - 3][j]; 
+
+      if (max < result) 
+          max = result; 
+      } 
+
+      // check the maximum product in 
+      // diagonal and anti - diagonal 
+      if ((i - 3) >= 0 && (j - 3) >= 0)  
+      { 
+      result = arr[i][j] * arr[i - 1][j - 1] *  
+              arr[i - 2][j - 2] * arr[i - 3][j - 3]; 
+
+      if (max < result) 
+          max = result; 
+      } 
+  } 
+  } 
+
+  return max; 
+} 
