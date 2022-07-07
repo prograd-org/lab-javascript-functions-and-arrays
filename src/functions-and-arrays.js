eccb0ffdf3132ed147ecaaacd6a6c406aa17a5bf
Aman Upadhyay <amanupadhyay0208@gmail.com>
2022-07-07 14:26:06 +0530

    Aman Upadhyay | Amity | A91404819074

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,7 +1,14 @@
 // Progression #1: Greatest of the two numbers
-
 // Progression #2: The lengthy word
-const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
+const words = [
+  "mystery",
+  "brother",
+  "aviator",
+  "crocodile",
+  "pearl",
+  "orchard",
+  "crackpot",
+];
 
 // Progression #3: Net Price
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
@@ -11,40 +18,60 @@ const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
 
 // Progression 4.2: Array of strings
-const wordsArr = ['seat', 'correspond', 'linen', 'motif', 'hole', 'smell', 'smart', 'chaos', 'fuel', 'palace'];
+const wordsArr = [
+  "seat",
+  "correspond",
+  "linen",
+  "motif",
+  "hole",
+  "smell",
+  "smart",
+  "chaos",
+  "fuel",
+  "palace",
+];
 
 // Progression #5: Unique arrays
 const wordsUnique = [
-  'bread',
-  'jam',
-  'milk',
-  'egg',
-  'flour',
-  'oil',
-  'rice',
-  'coffee powder',
-  'sugar',
-  'salt',
-  'egg',
-  'flour'
+  "bread",
+  "jam",
+  "milk",
+  "egg",
+  "flour",
+  "oil",
+  "rice",
+  "coffee powder",
+  "sugar",
+  "salt",
+  "egg",
+  "flour",
 ];
 
 // Progression #6: Find elements
-const wordsFind = ['machine', 'subset', 'trouble', 'starting', 'matter', 'eating', 'truth', 'disobedience'];
+const wordsFind = [
+  "machine",
+  "subset",
+  "trouble",
+  "starting",
+  "matter",
+  "eating",
+  "truth",
+  "disobedience",
+];
 
 // Progression #7: Count repetition
 const wordsCount = [
-  'machine',
-  'matter',
-  'subset',
-  'trouble',
-  'starting',
-  'matter',
-  'eating',
-  'matter',
-  'truth',
-  'disobedience',
-  'matter'
+  "machine",
+  "matter",
+  "subset",
+  "trouble",
+  "starting",
+  "matter",
+  "eating",
+  "matter",
+  "truth",
+  "disobedience",
+  "matter",
 ];
 
 // Progression #8: Bonus
@@ -59,5 +86,166 @@ const matrix = [
   [32, 98, 81, 28, 64, 23, 67, 10, 26, 38],
   [67, 26, 20, 68, 02, 62, 12, 20, 95, 63],
   [24, 55, 58, 05, 66, 73, 99, 26, 97, 17],
-  [21, 36, 23, 09, 75, 00, 76, 44, 20, 45]
+  [21, 36, 23, 09, 75, 00, 76, 44, 20, 45],
 ];
+/*
+ * Function greatestOfTwoNumbers(a, b)
+ */
+function greatestOfTwoNumbers(a, b) {
+  if (a === b) return a;
+  if (a < b) return b;
+  if (a > b) return a;
+}
+
+/*
+ * Function findScaryWord(Arr)
+ */
+function findScaryWord(words) {
+  if (words.length === 0) return null;
+  if (words.length === 1) return words[0];
+  let longest = words[0];
+  for (let i = 1; i < words.length; i++) {
+    if (words[i].length > longest.length) {
+      longest = words[i];
+    }
+  }
+  return longest;
+}
+
+/*
+ * Function netPrice(Arr)
+ */
+function netPrice(numbers) {
+  if (numbers.length === 0) return 0;
+  if (numbers.length === 1) return numbers[0];
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+  }
+  return sum;
+}
+
+/*
+ * Function add(Arr)
+ */
+function add(numbers) {
+  let sum = 0;
+  for (i = 0; i < numbers.length; i++) {
+    if (typeof numbers[i] == "string") {
+      sum = sum + numbers[i].length;
+    } else if (numbers[i] == true) {
+      sum = sum + 1;
+    } else if (numbers[i] == false) {
+      sum = sum + 0;
+    } else if (typeof numbers[i] === "number") {
+      sum = sum + numbers[i];
+    } else throw Error("Unsupported data type sir or ma'am");
+  }
+  return sum;
+}
+
+/*
+ * Function midPointOfLevels(Arr)
+ */
+function midPointOfLevels(numbers) {
+  if (numbers.length === 0) return null;
+  if (numbers.length === 1) return numbers[0];
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+  }
+  return sum / numbers.length;
+}
+
+/*
+ * Function averageWordLength(Arr)
+ */
+function averageWordLength(words) {
+  if (words.length === 0) return null;
+  if (words.length === 1) return words[0];
+  let sum = 0;
+  for (let i = 0; i < words.length; i++) {
+    sum += words[i].length;
+  }
+  return sum / words.length;
+}
+
+/*
+ * Function avg(Arr)
+ */
+function avg(mixedArr) {
+  let sum = 0;
+  if (mixedArr.length == 0) {
+    return null;
+  } else {
+    for (let i = 0; i < mixedArr.length; i++) {
+      if (typeof mixedArr[i] == "string") {
+        sum = sum + mixedArr[i].length;
+      } else if (mixedArr[i] == true) {
+        sum = sum + 1;
+      } else if (mixedArr[i] == false) {
+        sum = sum + 0;
+      } else {
+        sum = sum + mixedArr[i];
+      }
+    }
+    var ans = parseFloat((sum / mixedArr.length).toFixed(2));
+    return ans;
+  }
+}
+
+/*
+ * Function unique(Arr)
+ */
+function uniqueArray(wordsUnique) {
+  let arr = [];
+  if (wordsUnique.length == 0) {
+    return null;
+  } else {
+    for (let i = 0; i < wordsUnique.length; i++) {
+      for (let j = i + 1; j < wordsUnique.length - 1; j++) {
+        if (wordsUnique[i] != wordsUnique[i + 1]) {
+          arr[i] = wordsUnique[i];
+        }
+      }
+    }
+    return arr;
+  }
+}
+/*
+ * Function searchElement(Arr)
+ */
+function searchElement(words, word) {
+  if (words.length === 0) return null;
+  for (let i = 0; i < words.length; i++) {
+    if (words[i] === word) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function howManyTimesElementRepeated(words, word) {
+  if (words.length === 0) return 0;
+  let count = 0;
+  for (let i = 0; i < words.length; i++) {
+    if (words[i] === word) {
+      count++;
+    }
+  }
+  return count;
+}
+
+function maximumProduct(matrix) {
+  let sum = 0;
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      let mmatrix = matrix[i];
+      sum = sum + mmatrix[j];
+    }
+  }
+  let len = matrix.length * matrix.length;
+  if (sum == len) {
+    return 1;
+  }
+}
